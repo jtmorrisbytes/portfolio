b6fe1f41625b8b7a805ec3e5642f7930a8fa4c9f
Jordan Morris <jthecybertinkerer@gmail.com>
2021-04-15 00:49:32 -0500

    added screenshots"

--- createScreenshots.js
+++ createScreenshots.js
@@ -5,22 +5,32 @@ const path = require("path");
     headless: false,
     args: [`--window-size=1600,1600`],
   });
-  for (let width of [300, 600, 960,1200]) {
-    const [page] = await browser.pages();
-    await page.setViewport({ width: width, height: width });
-    await page.goto(process.env.PUBLIC_URL);
-    await page.waitForResponse(`${process.env.PUBLIC_URL}/api/user`);
-    await page.waitForResponse(`${process.env.PUBLIC_URL}/api/skills`);
-    await page.waitForResponse(
-      `${process.env.PUBLIC_URL}/api/projects?first=10`,
-      {
-        timeout: 5000,
-      }
-    );
+  const [page] = await browser.pages();
+  await page.goto(process.env.PUBLIC_URL);
+  await page.waitForResponse(`${process.env.PUBLIC_URL}/api/user`);
+  await page.waitForResponse(`${process.env.PUBLIC_URL}/api/skills`);
+  await page.waitForResponse(
+    `${process.env.PUBLIC_URL}/api/projects?first=10`,
+    {
+      timeout: 5000,
+    }
+  );
+  for (let [width, height] of [
+    [300, 300],
+    [600, 600],
+    [960, 960],
+    [1200, 1200],
+    [1200, 630],
+  ]) {
+    await page.setViewport({ width: width, height: height });
     await page.waitForTimeout(1000);
     await page.screenshot({
       type: "png",
-      path: path.join(process.cwd(), "public", `preview-${width}.png`),
+      path: path.join(
+        process.cwd(),
+        "preview",
+        `preview-${width}x${height}.png`
+      ),
     });
   }
   await browser.close();
